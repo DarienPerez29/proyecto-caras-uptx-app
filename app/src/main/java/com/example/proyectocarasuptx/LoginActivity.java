c0c565fdcef27f22e058763472ed65f845074d91
Darien Pérez Cano <33855580+Darko290@users.noreply.github.com>
2021-10-01 11:49:16 -0500

    Se agregan constantes para dimensiones de margenes, espacio negativo y se corrige el tamaño de los iconos.

--- app/src/main/java/com/example/proyectocarasuptx/LoginActivity.java
+++ app/src/main/java/com/example/proyectocarasuptx/LoginActivity.java
@@ -12,31 +12,30 @@ import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
 
+    TextInputLayout userField, passField;
+    MaterialButton loginBtn, fpassBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        TextInputLayout userField = (TextInputLayout) findViewById(R.id.username_field);
-        TextInputLayout passField = (TextInputLayout) findViewById(R.id.password_field);
+        userField = findViewById(R.id.username_field);
+        passField = findViewById(R.id.password_field);
 
-        MaterialButton loginBtn = (MaterialButton) findViewById(R.id.login_btn);
-        MaterialButton fpassBtn = (MaterialButton) findViewById(R.id.forget_pass_btn);
+        loginBtn = findViewById(R.id.login_btn);
+        fpassBtn = findViewById(R.id.forget_pass_btn);
 
-        try{
-            loginBtn.setOnClickListener(v -> {
-                String username = Objects.requireNonNull(userField.getEditText()).getText().toString();
-                String password = Objects.requireNonNull(passField.getEditText()).getText().toString();
+        loginBtn.setOnClickListener(v -> {
+            String username = Objects.requireNonNull(userField.getEditText()).getText().toString();
+            String password = Objects.requireNonNull(passField.getEditText()).getText().toString();
 
-                if(username.equals("admin") && password.equals("pass")){
-                    Intent load = new Intent(LoginActivity.this, DashboardActivity.class);
-                    startActivity(load);
-                    finish();
-                }
-            });
-        }catch(Exception e){
-            System.out.println(e);
-        }
+            //if(username.equals("admin") && password.equals("pass")){
+                Intent load = new Intent(LoginActivity.this, DashboardActivity.class);
+                startActivity(load);
+                finish();
+            //}
+        });
 
 
     }
