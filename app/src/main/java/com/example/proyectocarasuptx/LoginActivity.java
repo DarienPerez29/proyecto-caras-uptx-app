a08ded0eae7db1c79d314348881d4188f931672c
Darien Pérez Cano <33855580+Darko290@users.noreply.github.com>
2021-10-04 04:38:46 -0500

    Se añaden dialogs como advertencias, se agrega la funcionalidad de mensajes de alerta y validación básica de campos del login

--- app/src/main/java/com/example/proyectocarasuptx/LoginActivity.java
+++ app/src/main/java/com/example/proyectocarasuptx/LoginActivity.java
@@ -2,8 +2,13 @@ package com.example.proyectocarasuptx;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputLayout;
@@ -12,31 +17,103 @@ import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
 
-    TextInputLayout userField, passField;
-    MaterialButton loginBtn, fpassBtn;
+    TextInputLayout userLayout, passLayout;
+    MaterialButton loginBtn, forgottenAccountBtn;
+    Dialog loginErrorDialog, forgottenAccountDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        userField = findViewById(R.id.username_field);
-        passField = findViewById(R.id.password_field);
-
+        userLayout = findViewById(R.id.username_layout);
+        passLayout = findViewById(R.id.password_layout);
         loginBtn = findViewById(R.id.login_btn);
-        fpassBtn = findViewById(R.id.forget_pass_btn);
+        forgottenAccountBtn = findViewById(R.id.forget_pass_btn);
 
+        loginErrorDialog = new Dialog(this);
+        forgottenAccountDialog = new Dialog(this);
+
+        // Field watchers
+        Objects.requireNonNull(userLayout.getEditText()).addTextChangedListener(userFieldWatcher);
+        Objects.requireNonNull(passLayout.getEditText()).addTextChangedListener(passFieldWatcher);
+
+        // Login button action
         loginBtn.setOnClickListener(v -> {
-            String username = Objects.requireNonNull(userField.getEditText()).getText().toString();
-            String password = Objects.requireNonNull(passField.getEditText()).getText().toString();
-
-            //if(username.equals("admin") && password.equals("pass")){
-                Intent load = new Intent(LoginActivity.this, DashboardActivity.class);
-                startActivity(load);
-                finish();
-            //}
+            if (Validate.emptyFields(userLayout, passLayout)) loginAtempt();
         });
 
-
+        // Forgotten account button action
+        forgottenAccountBtn.setOnClickListener(v -> showForgottenAcountMessage());
     }
+
+    // Login atempt
+    public void loginAtempt() {
+        if (Validate.dataFields(userLayout, passLayout)) startNextAtempt();
+        else showLoginError();
+    }
+
+    // Start next activity
+    public void startNextAtempt() {
+        Intent load = new Intent(LoginActivity.this, DashboardActivity.class);
+        startActivity(load);
+        finish();
+    }
+
+    // Show dialog if login fails
+    public void showLoginError(){
+        loginErrorDialog.setContentView(R.layout.dialog_login_error);
+        loginErrorDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        MaterialButton confirmBtn = loginErrorDialog.findViewById(R.id.login_error_btn);
+
+        confirmBtn.setOnClickListener(v -> loginErrorDialog.dismiss());
+
+        loginErrorDialog.show();
+    }
+
+    // Show dialog for forgoten password
+    public void showForgottenAcountMessage(){
+        forgottenAccountDialog.setContentView(R.layout.dialog_forgotten_account);
+        forgottenAccountDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        MaterialButton confirmBtn = forgottenAccountDialog.findViewById(R.id.forgotten_account_btn);
+
+         confirmBtn.setOnClickListener(v -> forgottenAccountDialog.dismiss());
+
+         forgottenAccountDialog.show();
+    }
+
+    // Watcher for username field
+    private final TextWatcher userFieldWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            Validate.clearEmptyErrorWatcher(userLayout);
+        }
+    };
+
+    // Watcher for password field
+    private final TextWatcher passFieldWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            Validate.clearEmptyErrorWatcher(passLayout);
+        }
+    };
 }
---
+++ app/src/main/java/com/example/proyectocarasuptx/Validate.java
@@ -0,0 +1,41 @@
+package com.example.proyectocarasuptx;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Objects;
+
+
+public class Validate {
+    private static final String EMPTY_ERR = "Este campo no puede estar vacío...";
+
+    // Verify all data
+    public static boolean dataFields(TextInputLayout userLayout, TextInputLayout passLayout) {
+        String user = Objects.requireNonNull(userLayout.getEditText()).getText().toString().trim();
+        String pass = Objects.requireNonNull(passLayout.getEditText()).getText().toString().trim();
+
+        return user.equals("user") && pass.equals("pass");
+    }
+
+    // Verify if fields are empty
+    public static boolean emptyFields(TextInputLayout userLayout, TextInputLayout passLayout) {
+        String user = Objects.requireNonNull(userLayout.getEditText()).getText().toString().trim();
+        String pass = Objects.requireNonNull(passLayout.getEditText()).getText().toString().trim();
+
+        if (!user.isEmpty() && !pass.isEmpty()) return true;
+
+        if (user.isEmpty()) userLayout.setError(EMPTY_ERR);
+        else userLayout.setErrorEnabled(false);
+
+        if (pass.isEmpty()) passLayout.setError(EMPTY_ERR);
+        else passLayout.setErrorEnabled(false);
+
+        return false;
+    }
+
+    // Clear error watcher
+    public static void clearEmptyErrorWatcher(TextInputLayout layout) {
+        String content = Objects.requireNonNull(layout.getEditText()).getText().toString().trim();
+        if (!content.isEmpty()) layout.setErrorEnabled(false);
+    }
+
+}
